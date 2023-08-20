2c8015fe5de287a2ecc03eb3877adf6910393d94
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2023-08-20 12:54:34 +0300

    add auth page base

--- src/Pages/AuthPagesBase/AuthPagesBase.js
+++ src/Pages/AuthPagesBase/AuthPagesBase.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useNavigate, Outlet } from "react-router-dom";
 import { fetchUserData } from "../../Services/authServices";
 import UserContext from "../../Contexts/UserContext";
 
 export default function AuthPageBase() {
   const [user, setUser] = useState({});
+  const navigate = useNavigate();
   useEffect(() => {
-    // fetchUserData().then((data) => setUser(data));
+    fetchUserData()
+      .then((data) => setUser(data))
+      .catch((err) => navigate("/login"));
   }, []);
+  console.log(user);
 
   return (
     <UserContext.Provider value={user}>
