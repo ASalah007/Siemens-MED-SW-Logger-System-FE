3a2bc7a440ce817c3fc197fd3243bfed99cc81b8
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2023-08-22 12:16:45 +0300

    fix reloading issue

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -1,28 +1,27 @@
 import React, { useEffect } from "react";
-import LoginForm from "../../Components/AuthForm/LoginForm"
+import LoginForm from "../../Components/AuthForm/LoginForm";
 import AuthBanner from "../../Components/AuthBanner/AuthBanner";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import UserContext from "../../Contexts/UserContext";
-import 'animate.css';
+import "animate.css";
 
 function LoginPage() {
   const navigate = useNavigate();
   const user = React.useContext(UserContext);
 
   useEffect(() => {
-    if(user){
+    if (Object.keys(user).length) {
       navigate("/");
     }
   }, []);
 
-
   return (
     <div className="w-full min-h-screen bg-white flex items-center justify-center">
-    <div className="w-2/4 animate__animated animate__fadeInLeft">
+      <div className="w-2/4 animate__animated animate__fadeInLeft">
         <LoginForm />
-    </div>
+      </div>
 
-    <AuthBanner/>
+      <AuthBanner />
     </div>
   );
 }
